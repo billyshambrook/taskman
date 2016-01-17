d98c5519277fbec052515a6bd76d8e1e62a2ef0e
Billy Shambrook <billy.shambrook@gmail.com>
2016-01-17 01:19:33 +0000

    Add documentation.

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import sys, os
+
+sys.path.insert(0, os.path.abspath('..'))
+import taskman
+
+
+extensions = ['sphinx.ext.autodoc']
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = u'taskman'
+copyright = u'2011, Billy Shambrook'
+version = taskman.__version__
+release = taskman.__version__
+
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+html_theme = 'default'
+html_static_path = ['_static']
+
+htmlhelp_basename = 'taskmanpydoc'
+
+
+latex_documents = [
+  ('index', 'taskman.tex', u'Taskman Documentation',
+   u'Billy Shambrook', 'manual'),
+]
+
+
+man_pages = [
+    ('index', 'taskman', u'Taskman Documentation',
+     [u'Billy Shambrook'], 1)
+]
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+Taskman
+=======
+
+Overview
+--------
+
+A python task manager using Consul for it's backend.
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search
