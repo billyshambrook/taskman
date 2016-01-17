3d24a3318aa32a6cd332591261260075293cece1
Billy Shambrook <billy.shambrook@gmail.com>
2016-01-17 00:49:15 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='taskman',
-    version='0.0.1',
+    version='0.0.2',
     license='BSD',
     url='http://github.com/billyshambrook/taskman',
     author='Billy Shambrook',
